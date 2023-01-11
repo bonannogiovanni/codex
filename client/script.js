729759fac6db8cec4731981ac8fc7fc9d421b698
bonannogiovanni <bonannogiovanni@hotmail.com>
2023-01-11 14:40:48 +0100

    Update the server url

--- client/script.js
+++ client/script.js
@@ -71,7 +71,8 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv)
 
-  const response = await fetch('http://localhost:5000', {
+  // const response = await fetch('http://localhost:5000', {
+  const response = await fetch(' https://codex-lw8j.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
